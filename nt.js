b1db2c4a731ec24886f1809a312431aa4663c5c1
Dylan Nelson <nlsdyl001@myuct.ac.za>
2021-03-18 21:22:10 +0200

    add list of small primes and do trial division

--- nt.js
+++ nt.js
@@ -1,3 +1,5 @@
+const smallPrimes = [2n, 3n, 5n, 7n, 11n, 13n, 17n, 19n, 23n, 29n, 31n, 37n, 41n, 43n, 47n, 53n, 59n, 61n, 67n, 71n, 73n, 79n, 83n, 89n, 97n]
+
 function jacobiSymbol(a, b) {
     if (b < 0n) {
         throw "b must be positive"
@@ -26,4 +28,14 @@ function jacobiSymbol(a, b) {
         return 0
     }
     return flip ? -1n : 1n
+}
+
+function isPrime(num) {
+    for (const p of smallPrimes) {
+        if (num == p) return true
+        if (num % p == 0) {
+            return false
+        }
+    }
+    return undefined
 }
