bec318df3fc4bd4a2915d517472fd46833ffb954
Dylan Nelson <nlsdyl001@myuct.ac.za>
2021-03-18 21:42:29 +0200

    add modular exponentiation function

--- nt.js
+++ nt.js
@@ -1,5 +1,22 @@
 const smallPrimes = [2n, 3n, 5n, 7n, 11n, 13n, 17n, 19n, 23n, 29n, 31n, 37n, 41n, 43n, 47n, 53n, 59n, 61n, 67n, 71n, 73n, 79n, 83n, 89n, 97n]
 
+function modPow(base, exp, modulus) {
+    var mask = 1n
+    while (mask <= exp) {
+        mask <<= 1n
+    }
+    mask >>= 1n
+    var result = 1n
+    while (mask > 0n) {
+        result = (result * result) % modulus
+        if (exp & mask) {
+            result = (result * base) % modulus
+        }
+        mask >>= 1n
+    }
+    return result
+}
+
 function jacobiSymbol(a, b) {
     if (b < 0n) {
         throw "b must be positive"
@@ -33,7 +50,7 @@ function jacobiSymbol(a, b) {
 function isPrime(num) {
     for (const p of smallPrimes) {
         if (num == p) return true
-        if (num % p == 0) {
+        if (num % p == 0n) {
             return false
         }
     }
